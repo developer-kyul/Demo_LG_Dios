248fae3b2feeb6cb8d651202bed42e026b7518fb
kyul <developer.kyul@gmail.com>
2023-07-08 18:02:50 +0900

    [feat] 메인 이미지 수정

--- js/main.js
+++ js/main.js
@@ -30,6 +30,20 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
     //loop move
+    gsap.to('.fix_title .inner',{
+        x: -4436,
+        duration: 30,
+        ease: 'none',
+        repeat: -1
+    })
+
+    gsap.to('.page02_letter .inner',{
+        y: -3086,
+        duration: 30,
+        ease: 'none',
+        repeat: -1
+    })
+
     gsap.to('.left_plant1',{ //선택자
         rotate: -2, //구동
         duration: 4, //시간
@@ -102,6 +116,79 @@ window.addEventListener('DOMContentLoaded', function () {
         repeat: -1
     })
 
+    gsap.to('.left_kitchen1 img',{
+        y: -40, /*ex) y축으로 음수50px 만큼움직이겠다.*/
+        duration: 4, //속도
+        yoyo: true, //왕복
+        ease: 'none', //가속도
+        repeat: -1 //반복
+    })
+    
+    gsap.to('.left_kitchen2 img',{
+        y: -40,
+        duration: 4,
+        yoyo: true,
+        ease: 'none',
+        repeat: -1 
+    })
+
+    gsap.to('.left_kitchen3 img',{
+        y: -40,
+        duration: 4,
+        yoyo: true,
+        ease: 'none',
+        repeat: -1 
+    })
+
+    gsap.to('.left_kitchen4 img',{
+        y: -40,
+        duration: 4,
+        yoyo: true,
+        ease: 'none',
+        repeat: -1 
+    })
+
+    gsap.to('.left_kitchen5 img',{
+        y: -40,
+        duration: 4,
+        yoyo: true,
+        ease: 'none',
+        repeat: -1 
+    })
+
+    gsap.to('.right_kitchen1 img',{
+        y: -40,
+        duration: 4,
+        yoyo: true,
+        ease: 'none',
+        repeat: -1 
+    })
+
+    gsap.to('.right_kitchen2 img',{
+        y: -40,
+        duration: 4,
+        yoyo: true,
+        ease: 'none',
+        repeat: -1 
+    })
+    
+    gsap.to('.right_kitchen3 img',{
+        y: -40,
+        duration: 4,
+        yoyo: true,
+        ease: 'none',
+        repeat: -1 
+    })
+
+    gsap.to('.right_kitchen4 img',{
+        y: -40,
+        duration: 4,
+        yoyo: true,
+        ease: 'none',
+        repeat: -1 
+    })
+
+
     /*  slide,click event section */
     gsap.to('.fix_this_1', { //대상
         scrollTrigger: {
@@ -161,9 +248,23 @@ window.addEventListener('DOMContentLoaded', function () {
         onLeave: ()=>{
             $('.fix_this_1').css('background','#c98181')
             $('#section01 .main_bg .main_img01 img').attr('src',`../image/section01/2_kitchen/kitchen_main.png`)
-            // $('#section01 .left_plant1 img').attr('src',`../image/section01/2_kitchen/lemon.png`)
         }
     });
+
+
+/*  slide,click event section */
+gsap.to('.fix_this_1', { //대상
+    scrollTrigger: {
+        trigger: ".trigger_this_01", //기준
+        start: "top top", //기준점 브라우저
+        // end: () => "+=" + 300,
+        end: 'bottom bottom', //기준점 브라우저
+        pin: true, //고정
+        pinSpacing: false,
+        scrub: true,
+    }
+});
+
 
     //('선택자',{구동},듀레이션(0~1))
     sc01_scale02.to("#section01 .main_bg",{scale:1},0);
@@ -238,18 +339,30 @@ window.addEventListener('DOMContentLoaded', function () {
         animation: sc01_laundry_box,
         trigger: "#section01", //기준
         start: "20500px top", //기준점 브라우저
-        end: "21500px bottom", //기준점 브라우저
+        end: "21000px bottom", //기준점 브라우저
         scrub: 1,
     });
 
     //('선택자',{구동},듀레이션(0~1))
     sc01_laundry_box.to("#section01 .left_laundry1",{x:0 ,y:0},0);
-
-
-
-
-
-
+    sc01_laundry_box.to("#section01 .left_laundry2",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .left_laundry3",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .left_laundry4",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .left_laundry5",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .left_laundry6",{x:0 ,y:-600},0);
+    sc01_laundry_box.to("#section01 .left_laundry6",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .left_laundry7",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .left_laundry8",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .left_laundry9",{x:0 ,y:0},0);
+    
+    sc01_laundry_box.to("#section01 .right_laundry1",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .right_laundry2",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .right_laundry3",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .right_laundry4",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .right_laundry5",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .right_laundry6",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .right_laundry7",{x:0 ,y:0},0);
+    sc01_laundry_box.to("#section01 .right_laundry8",{x:0 ,y:0},0);
 
     let sc01_scale05 = gsap.timeline();
     ScrollTrigger.create({
@@ -262,10 +375,24 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //('선택자',{구동},듀레이션(0~1))
     sc01_scale05.to("#section01 .main_bg",{scale:0.54},0);
-    sc01_scale05.to("#section01 .left_laundry1",{x:-300 ,y:-500},0);
+    sc01_scale05.to("#section01 .left_laundry1",{x:0 ,y:-500},0.1);
+    sc01_scale05.to("#section01 .left_laundry2",{x:300 ,y:-500},0.1);
+    sc01_scale05.to("#section01 .left_laundry3",{x:-300 ,y:-500},0.1);
+    sc01_scale05.to("#section01 .left_laundry4",{x:-1550 ,y:0},0.1);
+    sc01_scale05.to("#section01 .left_laundry5",{x:0 ,y:-1800},0.1);
+    sc01_scale05.to("#section01 .left_laundry6",{x:0 ,y:-1800},0.11);
+    sc01_scale05.to("#section01 .left_laundry7",{x:0 ,y:-700},0.1);
+    sc01_scale05.to("#section01 .left_laundry8",{x:200 ,y:-1000},0.1);
+    sc01_scale05.to("#section01 .left_laundry9",{x:0 ,y:-800},0.18);
 
-
-
+    sc01_scale05.to("#section01 .right_laundry1",{x:-300 ,y:-500},0.1);
+    sc01_scale05.to("#section01 .right_laundry2",{x:0 ,y:-1700},0.1);
+    sc01_scale05.to("#section01 .right_laundry3",{x:0 ,y:-1700},0.1);
+    sc01_scale05.to("#section01 .right_laundry4",{x:0 ,y:-1700},0.11);
+    sc01_scale05.to("#section01 .right_laundry5",{x:200 ,y:-500},0.1);
+    sc01_scale05.to("#section01 .right_laundry6",{x:200 ,y:0},0.1);
+    sc01_scale05.to("#section01 .right_laundry7",{x:-800, y:-1680, z:0},0.1);
+    sc01_scale05.to("#section01 .right_laundry8",{x:-0 ,y:-1000},0.12);
 
     
 
@@ -289,6 +416,8 @@ window.addEventListener('DOMContentLoaded', function () {
     //('선택자',{구동},듀레이션(0~1))
     sc01_scale06.to("#section01 .main_bg",{scale:1},0);
 
+
+
     let room_box = gsap.timeline();
     ScrollTrigger.create({
         animation: room_box,
@@ -300,6 +429,17 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //('선택자',{구동},듀레이션(0~1))
     room_box.to("#section01 .left_room1",{x:0 ,y:0},0);
+    room_box.to("#section01 .left_room2",{x:0 ,y:0},0);
+    room_box.to("#section01 .left_room3",{x:0 ,y:0},0);
+    room_box.to("#section01 .left_room4",{x:0 ,y:0},0);
+    room_box.to("#section01 .left_room5",{x:0 ,y:0},0);
+    room_box.to("#section01 .left_room6",{x:0 ,y:0},0);
+
+    room_box.to("#section01 .right_room1",{x:0 ,y:0},0);
+    room_box.to("#section01 .right_room2",{x:0 ,y:0},0);
+    room_box.to("#section01 .right_room3",{x:0 ,y:0},0);
+    room_box.to("#section01 .right_room4",{x:0 ,y:0},0);
+
 
 
     let sc01_top_bottom = gsap.timeline();
@@ -343,6 +483,42 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     se02_xScroll.to("#section02 .xScroll2",{left:'calc(100vw - 28798px)'},0);
+
+
+    gsap.to('.fix_this_4', {
+        scrollTrigger: {
+            trigger: ".trigger_this_04",
+            start: "top top",
+            // end: () => "+=" + 300,
+            end: 'bottom bottom',
+            pin: true,
+            pinSpacing: false,
+            scrub: true,
+        }
+    });
+
+    let se04_xScroll = gsap.timeline();
+    ScrollTrigger.create({
+        animation: se04_xScroll,
+        trigger: "#section04",
+        start: "50% top",
+        end: "bottom bottom",
+        scrub: 1,
+    });
+
+    se04_xScroll.to("#section04 .xScroll04",{left:'calc(100vw - 8726px)'},0);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
